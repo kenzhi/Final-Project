2d7fcb07108c41291d4e5fd918302884231c2811
kenzhi <minghuizhi@minghuois-MacBook-Pro.local>
2015-11-28 17:40:52 -0500

    homepage changed

--- js/self.js
+++ js/self.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $('.slider').slick({
         dots: true,
         lazyLoad: 'ondemand',
-        slidesToShow: 3,
+        slidesToShow: 4,
         slidesToScroll: 1
     });
 });
